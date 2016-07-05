15f03c7f4665a1e872c6139191a174797fc58e58
J Allen <jameson.f.allen@gmail.com>
2016-07-05 16:33:00 -0500

    Update narcissitic_number.pl
    refactored again

--- narcissitic_number.pl
+++ narcissitic_number.pl
@@ -1,7 +1,6 @@
 sub NarcissisticNumber {
-	($s, $f) = @_;
     @o=();
-    for ($s .. $f) {
+    for ( $_[0] .. $_[1] ) {
         $y=0;
         $y += $_**@x for (@x = split('',$_));
         push @o, int($_) if( $y == $_ );
