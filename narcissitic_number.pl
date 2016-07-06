37bffb42832e58f6a76239b060053d4c58f21d59
J Allen <jameson.f.allen@gmail.com>
2016-07-05 23:09:52 -0500

    Update narcissitic_number.pl
    adding challenge tasks

--- narcissitic_number.pl
+++ narcissitic_number.pl
@@ -1,3 +1,35 @@
+=pod
+
+A narcissistic number is a number that is equal to the sum of its digits each raised to the power equal to the total number of digits. For example, 153 is a narcissistic number, since 13 + 53 + 33 = 153.
+
+Given the numbers start and finish, return all narcissistic numbers in range [start, finish] (inclusive).
+
+Example
+
+For start = 100 and finish = 200, the output should be
+NarcissisticNumber(start, finish) = [153].
+
+153 is the only number in range [100, 200] which is narcissistic.
+
+Input/Output
+
+[time limit] 4000ms (perl)
+[input] integer start
+
+Constraints:
+1 ≤ start ≤ finish.
+
+[input] integer finish
+
+Constraints:
+start ≤ finish ≤ 104.
+
+[output] array.integer
+
+A sorted array of narcissistic numbers between start and finish (inclusive).
+
+=cut
+
 sub NarcissisticNumber {
     @o=();
     for ( $_[0] .. $_[1] ) {
